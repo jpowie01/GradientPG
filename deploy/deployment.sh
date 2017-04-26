ac603c283a54ec371a011db3b4a9de238e849042
Jakub Powierza <jpowie01@gmail.com>
2017-04-27 00:22:17 +0200

    Poprawka dla skryptu deploymentowego, który uruchamiał się w złym miejscu...

--- deploy/deployment.sh
+++ deploy/deployment.sh
@@ -4,7 +4,8 @@
 #
 
 # Definicje
-DEPLOYMENT_DIR=/srv/GradientPG/deploy
+MAIN_DIR=/srv/GradientPG
+DEPLOYMENT_DIR=$MAIN_DIR/deploy
 GIT_WRAPPER=$DEPLOYMENT_DIR/git_ssh_wrapper.sh
 LOG_DIR=$DEPLOYMENT_DIR/deployment_history.log
 
@@ -13,7 +14,7 @@ export HOME=/home/155173pj
 
 # Pullowanie z GitHuba
 echo "[`date`] Starting deployment." >> $LOG_DIR
-cd $DEPLOYMENT_DIR
+cd $MAIN_DIR
 GIT_SSH=$GIT_WRAPPER git pull >> $LOG_DIR
 GIT_SSH=$GIT_WRAPPER git submodule update --remote pliki >> $LOG_DIR
 echo "[`date`] End of deployment." >> $LOG_DIR
