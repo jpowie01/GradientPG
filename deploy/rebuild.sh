7d59437fb6a216502ec7d56a38bd87194a852ead
Jakub Powierza <jpowie01@gmail.com>
2017-11-05 21:54:23 +0100

    Lekkie czyszczenie

--- deploy/rebuild.sh
+++ deploy/rebuild.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-#
-# Skrypt odpala serwer w osobnym procesie jako root na porcie 80
-#
-
-# Definicje
 MAIN_DIR=/srv/GradientPG
 BUNDLE=/usr/local/bin/bundle
 JEKYLL=/home/155173pj/.gem/ruby/gems/jekyll-3.2.1/exe/jekyll
-
-# Tak dla pewności...
 export HOME=/home/155173pj
 
 # Budowanie strony
--- deploy/start_server.sh
+++ deploy/start_server.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-#
-# Skrypt odpala serwer w osobnym procesie jako root na porcie 80
-#
-
-# Definicje
 MAIN_DIR=/srv/GradientPG
 BUNDLE=/usr/local/bin/bundle
 JEKYLL=/home/155173pj/.gem/ruby/gems/jekyll-3.2.1/exe/jekyll
-
-# Tak dla pewności...
 export HOME=/home/155173pj
 
 # Uruchamienie serwera na porcie 80
 cd $MAIN_DIR
-sudo $BUNDLE exec $JEKYLL serve --host 172.20.83.26 --port 80 --detach --watch
+sudo $BUNDLE exec $JEKYLL serve --host 0.0.0.0 --port 80 --detach --watch
